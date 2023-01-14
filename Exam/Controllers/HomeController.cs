35c82d7189007a6b4296d43d8714f1266d3947cd
Aizel Nunag <izelllim@gmail.com>
2023-01-15 03:12:55 +0800

    save roadblock on getting EventID & EventName on Attendee.cshtml

--- Exam/Controllers/HomeController.cs
+++ Exam/Controllers/HomeController.cs
@@ -2,16 +2,19 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Exam.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Exam.Controllers
 {
     public class HomeController : Controller
     {
+        string key = "01cb716174f94156bd7d28c7faafedec"; // GET KEY FROM SITE  https://api.polzki.com/home/eventsapidocs click get your key Here
         public IActionResult Event()
         {
 
-            var str = ApiService.Get("https://api.polzki.com/api/events?key=01cb716174f94156bd7d28c7faafedec");
+            var str = ApiService.Get("https://api.polzki.com/api/events?key="+key);
 
+           // FOR POST
            // var jsonStr = "";
            // var strPost = ApiService.Post("https://api.polzki.com/api/events?key=01cb716174f94156bd7d28c7faafedec", jsonStr);
 
@@ -23,15 +26,21 @@ namespace Exam.Controllers
         public IActionResult Attendee()
         {
 
-            var str = ApiService.Get("https://api.polzki.com/api/attendees?key=01cb716174f94156bd7d28c7faafedec");
-
-            // var jsonStr = "";
-            // var strPost = ApiService.Post("https://api.polzki.com/api/attendees?key=01cb716174f94156bd7d28c7faafedec", jsonStr);
+            var str = ApiService.Get("https://api.polzki.com/api/attendees?key="+key);
 
             AttendeeResponse attendeeResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<AttendeeResponse>(str == null ? "" : str);
 
             return View(attendeeResponse);
         }
+        //public IActionResult UpdateAttendee()
+        //{
+        //     var jsonStr = "";
+        //     var strPost = ApiService.Post("https://api.polzki.com/api/updateattendee?key="+key+"&AttendeeID=[ATTENDEEID]&Name=[ATTENDEE_NAME]&EventID=[EVENTID]", jsonStr);
+
+        //    AttendeeResponse attendeeResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<AttendeeResponse>(strPost == null ? "" : strPost);
+
+        //    return View(attendeeResponse);
+        //}
 
         public IActionResult Contact()
         {
---
+++ Exam/Models/UpdateAttendee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exam.Models
+{
+    //public class UpdateAttendee
+    //{
+    //    //public string ID { get; set; }
+    //    //public string Name { get; set; }
+    //    //public string Email { get; set; }
+    //    //public string RegisterDate { get; set; }
+    //}
+}
